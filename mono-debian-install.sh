5072e1f1b2b2ea86974e9f4fe9e7af90639d3717
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-01 19:25:11 -0300

    Deixando logar tudo no console para todos os comandos.

--- mono-debian-install.sh
+++ mono-debian-install.sh
@@ -3,11 +3,11 @@
 PREFIX="/usr/local"
 PATH=$PREFIX/bin:$PATH
 
-sudo apt-get install wget git autoconf libtool automake build-essential gettext > /dev/null
+sudo apt-get install wget git autoconf libtool automake build-essential gettext
 
-git clone git://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1 > /dev/null
+git clone git://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1
 
-cd mono > /dev/null
+cd mono
 ./autogen.sh --prefix=$PREFIX \
 	--with-mcs-docs=no \
 	--with-xammac=no \
@@ -15,13 +15,12 @@ cd mono > /dev/null
 	--with-monodroid=no \
 	--with-profile2=no \
 	--with-profile4=no \
-	--with-profile4_5=yes \
-	> /dev/null
+	--with-profile4_5=yes
 
-make get-monolite-latest > /dev/null
-make > /dev/null
-sudo make install > /dev/null
+make get-monolite-latest
+make
+sudo make install
 
 # Instalando/atualizando os certificados SSL para habilitar as requisicoes HTTPS futuras
-sudo mozroots --import --machine --sync > /dev/null
-yes | sudo certmgr -ssl -m "https://www.myget.org" > /dev/null
+sudo mozroots --import --machine --sync
+yes | sudo certmgr -ssl -m "https://www.myget.org"
