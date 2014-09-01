b4a3d5a2a029c72ffedccc66808d169a82ce079b
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-01 16:06:04 -0300

    Tentando novamente instalação em [/usr/local] (observe local sem a barra no final). Agora com get-monolite-latest ao invés de apt-get install mono-devel. Além de reescrita de comandos sudo só quando necessário

--- mono-debian-install.sh
+++ mono-debian-install.sh
@@ -5,21 +5,22 @@
 PREFIX=$@
 
 if [ -z $PREFIX ]; then
-  PREFIX="/opt/mono/"
+  PREFIX="/usr/local"
 fi
 
 # Garantindo permissao de escrita em $PREFIX
-sudo mkdir $PREFIX
-sudo chown -R `whoami` $PREFIX
+#sudo mkdir $PREFIX
+#sudo chown -R `whoami` $PREFIX
 
 # Garantindo que os pacotes necessarios estejam instalados
-sudo apt-get install git autoconf libtool automake build-essential mono-devel gettext
+sudo apt-get install git autoconf libtool automake build-essential gettext #mono-devel
 
 # Clonando, compilando e instalando o Mono em $PREFIX
 PATH=$PREFIX/bin:$PATH
 git clone https://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1
 
 cd mono
-./autogen.sh --prefix=$PREFIX
+./autogen.sh --prefix=$PREFIX --with-mcs-docs=no
+make get-monolite-latest
 make
-make install
+sudo make install
