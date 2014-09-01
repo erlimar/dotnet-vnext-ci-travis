da220f5763b1b4a53d83e8925d04d6424a045545
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-01 17:59:22 -0300

    Invertendo a modificação para não enviar saída para /dev/null, de [mono-debian-install.sh] para [.travis.yml]. Isso porque se o script ficar muito tempo sem enviar nada para a saída do console, o worker entenderá que o script parou e irá abortar o build.

--- mono-debian-install.sh
+++ mono-debian-install.sh
@@ -3,11 +3,11 @@
 PREFIX="/usr/local"
 PATH=$PREFIX/bin:$PATH
 
-sudo apt-get install wget git autoconf libtool automake build-essential gettext
+sudo apt-get install wget git autoconf libtool automake build-essential gettext > /dev/null
 
-git clone git://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1
+git clone git://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1 > /dev/null
 
-cd mono
+cd mono > /dev/null
 ./autogen.sh --prefix=$PREFIX \
 	--with-mcs-docs=no \
 	--with-xammac=no \
@@ -15,12 +15,13 @@ cd mono
 	--with-monodroid=no \
 	--with-profile2=no \
 	--with-profile4=no \
-	--with-profile4_5=yes
+	--with-profile4_5=yes \
+	> /dev/null
 
-make get-monolite-latest
-make
-sudo make install
+make get-monolite-latest > /dev/null
+make > /dev/null
+sudo make install > /dev/null
 
 # Instalando/atualizando os certificados SSL para habilitar as requisicoes HTTPS futuras
-sudo mozroots --import --machine --sync
-yes | sudo certmgr -ssl -m "https://www.myget.org"
+sudo mozroots --import --machine --sync > /dev/null
+yes | sudo certmgr -ssl -m "https://www.myget.org" > /dev/null
