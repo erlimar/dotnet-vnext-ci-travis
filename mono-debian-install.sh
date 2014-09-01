123a92a4e86891c3c7b297ac07fdcb5d78f909f6
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-01 15:04:20 -0300

    Inclusao de script separado para instalação do Mono em sistemas do tipo Debian `mono-debian-install.sh`. E ajuste em `.travis.yml` para usar o script separado de instalação.

---
+++ mono-debian-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Instalador do Mono em sistemas Debian
+
+PREFIX=$@
+
+if [ -z $PREFIX ]; then
+  PREFIX="~/.mono/"
+fi
+
+# Garantindo permissao de escrita em $PREFIX
+sudo mkdir $PREFIX
+sudo chown -R `whoami` $PREFIX
+
+# Garantindo que os pacotes necessarios estejam instalados
+sudo apt-get install git autoconf libtool automake build-essential mono-devel gettext
+
+# Clonando, compilando e instalando o Mono em $PREFIX
+PATH=$PREFIX/bin:$PATH
+git clone https://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1
+
+cd mono
+./autogen.sh --prefix=$PREFIX
+make
+make install
