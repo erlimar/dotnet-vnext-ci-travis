3b0b30100ef0f954771eb8919dfd667331f9086a
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-03 07:36:12 -0300

    Ajuste no código para diminuir a quantidade de LOG impressos no console durante a instalação do Mono

--- mono-debian-install.sh
+++ mono-debian-install.sh
@@ -2,10 +2,10 @@
 
 PREFIX="/usr/local"
 
-sudo apt-get install wget git autoconf libtool automake build-essential gettext > /dev/null
+sudo apt-get install wget git autoconf libtool automake build-essential gettext > /dev/null 2>&1
 
 PATH=$PREFIX/bin:$PATH
-git clone git://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1 > /dev/null
+git clone git://github.com/mono/mono.git --branch mono-3.8.0-branch --depth 1 > /dev/null 2>&1
 
 cd mono > /dev/null
 ./autogen.sh --prefix=$PREFIX \
@@ -17,15 +17,15 @@ cd mono > /dev/null
 	--with-profile4=yes \
 	--with-profile4_5=yes
 
-make get-monolite-latest
-make
-sudo make install
+make get-monolite-latest > /dev/null
+make > /dev/null
+sudo make install > /dev/null
 
 MOZROOTS="$PREFIX/lib/mono/4.5/mozroots.exe"
 CERTMGR="$PREFIX/lib/mono/4.5/certmgr.exe"
 
 # Instalando/atualizando os certificados SSL para habilitar as requisicoes HTTPS futuras
-sudo mono  $MOZROOTS --import --machine --sync
-yes | sudo mono $CERTMGR -ssl -m "https://www.myget.org"
+sudo mono  $MOZROOTS --import --machine --sync > /dev/null
+yes | sudo mono $CERTMGR -ssl -m "https://www.myget.org" > /dev/null
 
 cd .. > /dev/null
